9f56715497ddaa1facbebd79d4a0ad4073bfb36a
Napat-Edu <omegyzr@gmail.com>
2024-01-23 14:22:06 +0700

    {UPDATE] change text alert

--- components/CareerResult.tsx
+++ components/CareerResult.tsx
@@ -101,7 +101,7 @@ export default function CareerResult(props: ICareerResult) {
             <Alert className="flex flex-row gap-3 p-4 items-start">
                 <img src="lightbulb-icon.svg" alt="lightbulb-icon" width={16} height={16} />
                 <AlertDescription>
-                    สายอาชีพที่ทำนายเป็นเพียงการนำข้อมูลที่ผู้ใช้กรอกมาหาสายอาชีพที่เหมาะสม ยังสายมีอาชีพอื่น ๆ ที่คุณสามารถเป็นได้ตามความต้องการของคุณ
+                    สายอาชีพที่ทำนายเป็นเพียงการนำข้อมูลที่ผู้ใช้กรอกมาหาสายอาชีพที่เหมาะสม ยังสายมีอาชีพอื่น ๆ ที่คุณสามารถเป็นได้ตามความต้องการ
                 </AlertDescription>
             </Alert>
         </>
