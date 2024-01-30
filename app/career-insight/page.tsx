32d2f4e1e059f7867229a4e7f91073dbaa931bcb
Napat-Edu <omegyzr@gmail.com>
2024-01-30 17:33:08 +0700

    [ADD] select component

--- app/career-insight/page.tsx
+++ app/career-insight/page.tsx
@@ -1,10 +1,15 @@
+import { InsightSelect } from "@/components/InsightSelect";
+
 export default function CareerInsightPage() {
     return (
         <section className="w-full px-6">
-            <section className="border-b-2 border-[#E4E4E7] pt-6 pb-4 flex flex-col gap-1">
+            <section className="border-b-2 border-[#E4E4E7] pt-6 pb-4 flex flex-col gap-1 mb-5">
                 <h1 className="text-primary font-bold text-4xl">Career Insight</h1>
                 <p className="text-[#71717A]">แสดงผลข้อมูลเชิงลึกของอาชีพที่คุณทำนายได้</p>
             </section>
+
+            <InsightSelect></InsightSelect>
+
         </section>
     );
 };
