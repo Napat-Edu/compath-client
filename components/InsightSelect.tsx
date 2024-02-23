39a077e9a705fe7d7ce7bf268431f8a67c2f2056
Rotii <niwatchai.wang@mail.kmutt.ac.th>
2024-02-23 15:18:57 +0700

    [UPDATE] add image

--- components/InsightSelect.tsx
+++ components/InsightSelect.tsx
@@ -11,6 +11,7 @@ import {
 import useLocalStorage from "./hooks/useLocalStorage";
 import useSelectInsight from "./hooks/useSelectInsight";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export function InsightSelect() {
     const localStorage = useLocalStorage();
@@ -27,6 +28,15 @@ export function InsightSelect() {
         return date.toDateString();
     };
 
+    const displayTime = (dateString: string) => {
+        const date = new Date(dateString);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const time = `${hours}:${minutes}:${seconds}`;
+        return time;
+    }
+
     useEffect(() => {
         if (localStorage.isStorageReady && localStorage.predictionHistory.length) {
             const latestHistory = localStorage.getLatestHistory();
@@ -38,7 +48,14 @@ export function InsightSelect() {
     return (
         localStorage.isStorageReady &&
         <Select onValueChange={(id) => { handleHistoryChange(id) }} value={currentSelectCareer}>
-            <SelectTrigger className="w-52">
+            <Image
+            src="resume.svg"
+            alt="resume icon"
+            height={0}
+            width={0}
+            className="mx-auto h-[32px] w-auto" />
+            <h3 className="text-center">การ์ดสายทำนาย</h3>
+            <SelectTrigger className="w-[492px] py-2">
                 <SelectValue placeholder="เลือกการ์ดทำนาย" />
             </SelectTrigger>
             <SelectContent>
@@ -48,7 +65,7 @@ export function InsightSelect() {
                         localStorage.predictionHistory.map((history, idx) => {
                             return (
                                 <SelectItem value={history.objectId} key={"history-" + history.result + idx}>
-                                    {history.result} - {displayDate(history.submitDate)}
+                                    {history.result} - {displayTime(history.submitDate)} - {displayDate(history.submitDate)}
                                 </SelectItem>
                             );
                         })
