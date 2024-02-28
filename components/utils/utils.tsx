1e01ba9198cf014c50b5d833c13ce69d749cc06e
Rotii <niwatchai.wang@mail.kmutt.ac.th>
2024-02-28 16:51:47 +0700

    [ADD] Display time and date function

--- components/utils/utils.tsx
+++ components/utils/utils.tsx
@@ -3,3 +3,27 @@ export const toSalaryNumber = (salary: number): string => {
     const replacedSalary = salaryString.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     return replacedSalary;
 };
+
+export const displayDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toDateString();
+};
+
+export const displayTime = (dateString: string) => {
+    const date = new Date(dateString);
+    let ampm = 'AM'
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    if (hours == 12) {
+        ampm = 'PM'
+    } else if (hours == 0) {
+        hours = 12
+    } else if (hours > 12) {
+        hours -= 12
+        ampm = 'PM'
+    }
+
+    const time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+    return time;
+}
